31eea1f209d1636e1f597a88589a60cd9db65fba
Soprano <antfar67@gmail.com>
2023-05-08 22:03:23 -0700

    added DB functionality and set up SQL Alchemy ORM relationships

---
+++ app/models/User.py
@@ -0,0 +1,33 @@
+import bcrypt
+from app.db import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
+
+salt = bcrypt.gensalt()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
+
+    @validates('email')
+    def validate_email(self, key, email):
+        # make sure email address contains @ character
+        assert '@' in email
+
+        return email
+
+    @validates('password')
+    def validate_password(self, key, password):
+        assert len(password) > 4
+        # encrypt password
+        return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+# We add a new validate_email() method to the class that a @validates('email') decorator wraps.
+# The validate_email() method returns what the value of the email column should be, and the @validates() decorator internally handles the rest.
+# This decorator is similar to the @bp.routes() decorator that we used previously to handle the route functions.
+
+# The validate_email() method uses the assert keyword to check if an email address contains an at-sign character (@).
+# The assert keyword automatically throws an error if the condition is false, thus preventing the return statement from happening.
